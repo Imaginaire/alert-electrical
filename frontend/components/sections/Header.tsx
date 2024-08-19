23754a8b466935865371d88aacba4835cdeb4132
Jessy Yeh <jessy@imaginaire.co.uk>
2024-08-19 13:59:16 +0100

    add tag dropdown in header section

--- frontend/components/sections/Header.tsx
+++ frontend/components/sections/Header.tsx
@@ -1,19 +1,11 @@
-interface HeaderProps {
-  header: string
-  subHeader: string
-}
+import {Header as HeaderType} from '@/types'
 
-export default function Header(data: HeaderProps) {
-  const {header, subHeader} = data || {}
+export default function Header(data: HeaderType) {
+  const {header, headerTag} = data || {}
 
-  return (
-    <>
-      {data && (
-        <div className="header">
-          <h1>{header}</h1>
-          <p>{subHeader}</p>
-        </div>
-      )}
-    </>
-  )
+  const validHeaderTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const
+
+  const HeaderTag = validHeaderTags.includes(headerTag) ? headerTag : 'h1'
+
+  return <>{data && <HeaderTag>{header}</HeaderTag>}</>
 }
