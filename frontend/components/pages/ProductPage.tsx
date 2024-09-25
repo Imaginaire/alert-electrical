6f56eb1a6bf0c9e858578bb9d4b3f1e98b18daa2
dylanbrain <dylan@imaginaire.co.uk>
2024-09-25 15:57:30 +0100

    style: product page sticky image and refactor

--- frontend/components/pages/ProductPage.tsx
+++ frontend/components/pages/ProductPage.tsx
@@ -7,13 +7,11 @@ import Layout from '@/components/global/Layout'
 import Sections from '@/components/global/Sections'
 import {Variant} from '@/types/productType'
 import {PagePayload, ProductSettingPayload, SettingsPayload} from '@/types'
-
-import {Disclosure, DisclosureButton, DisclosurePanel} from '@headlessui/react'
-import {ChevronUpIcon, ChevronDownIcon, MinusIcon, PlusIcon} from '@heroicons/react/24/outline'
-import {CustomPortableText} from '../shared/CustomPortableText'
-import Link from 'next/link'
+import {MinusIcon, PlusIcon} from '@heroicons/react/24/outline'
 import {useCart} from '@/contexts/CartContext'
 import CartBanner from '../product/CartBanner'
+import LargeCta from '../sections/LargeCta'
+import DropDowns from '../shared/Dropdowns'
 
 export interface ProductPageProps {
   page: PagePayload | undefined
@@ -97,12 +95,13 @@ export default function ProductPage({
           <div className="productPage-container mx-auto max-w-2xl py-10 sm:py-24 lg:max-w-7xl lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
               {/* Image*/}
-              <div className="relative w-full h-[520px] lg:h-full">
+
+              <div className="relative lg:sticky lg:top-10 w-full h-[520px] ">
                 <Image
                   src={previewImageUrl || ''}
                   fill
                   alt={title || ''}
-                  sizes="100vw"
+                  sizes="50vw"
                   className="object-contain"
                 />
               </div>
@@ -187,75 +186,13 @@ export default function ProductPage({
                 </div>
 
                 {/* Additional details */}
-                <div className="divide-y divide-gray-200 border-b">
-                  {details.map((detail) => (
-                    <Disclosure key={detail.name} as="div">
-                      <h3>
-                        <DisclosureButton className="group relative flex w-full items-center justify-between py-4 px-5 text-left">
-                          <span className="text-xl  text-black group-data-[open]:text-secondary first-letter:uppercase">
-                            {detail.name}
-                          </span>
-                          <span className="ml-6 flex items-center">
-                            <ChevronDownIcon
-                              aria-hidden="true"
-                              className="block h-6 w-6 text-primary group-hover:text-gray-500 group-data-[open]:hidden"
-                            />
-                            <ChevronUpIcon
-                              aria-hidden="true"
-                              className="hidden h-6 w-6 text-secondary group-hover:text-secondary group-data-[open]:block"
-                            />
-                          </span>
-                        </DisclosureButton>
-                      </h3>
-                      <DisclosurePanel className="prose prose-sm pb-6 font-manrope">
-                        <ul role="list" className="px-5">
-                          {detail.items.map((item) => {
-                            if (typeof item === 'string') {
-                              return <li key={item}>{item}</li>
-                            }
-                            return item ? (
-                              <CustomPortableText
-                                value={item}
-                                linkClasses="text-secondary underline"
-                              />
-                            ) : null
-                          })}
-                        </ul>
-                      </DisclosurePanel>
-                    </Disclosure>
-                  ))}
-                </div>
+                {details && <DropDowns data={details} />}
               </div>
             </div>
           </div>
 
-          {cta && (
-            <section className="flex justify-center relative">
-              <Image
-                src={backgroundImgUrl || ''}
-                fill={true}
-                alt="Large CTA background image"
-                sizes="100vw"
-                className="object-cover object-center"
-                priority={true}
-                quality={100}
-              />
-              <div className="z-10 max-w-[600px] my-32 mx-5 p-[30px] sm:p-11 bg-primary text-white text-center">
-                {cta.header && <h2 className="text-[32px] leading-9 mb-3 sm:mb-7">{cta.header}</h2>}
-                {cta.description && (
-                  <div className="font-manrope font-light mb-3 sm:mb-7">
-                    <CustomPortableText
-                      value={cta.description}
-                      paragraphClasses="[&:not(last)]:mb-4"
-                    />
-                  </div>
-                )}
-                <Link href={cta.slug?.current || '/'}>
-                  <span className="text-xl underline uppercase">{cta.linkText}</span>
-                </Link>
-              </div>
-            </section>
-          )}
+          {/* Large CTA */}
+          {cta && <LargeCta {...cta} />}
 
           {/* Sections */}
           {sections && sections.length > 0 && <Sections sections={sections} settings={settings} />}
