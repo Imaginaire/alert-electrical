9d77661575140b5c1bb956346afc8143bc3a6079
dylanbrain <dylan@imaginaire.co.uk>
2024-10-25 15:08:54 +0100

    fix: redirects

--- frontend/shared/utils/generateRedirects.mjs
+++ frontend/shared/utils/generateRedirects.mjs
@@ -5,31 +5,37 @@ const client = createClient({
   projectId: 'a430q0zj',
   dataset: 'production',
   useCdn: false,
+  apiVersion: '2022-03-07',
 })
 
 const query = `
   *[_type == "redirections"].redirects[]{
-    source,
-    destination,
-    statusCode
+      source,
+      destination,
+      statusCode
   }
 `
 
 export default async function generateRedirects() {
   const redirects = await client.fetch(query)
 
-  // Filter out invalid redirects (those missing 'source', 'destination', or 'permanent')
-  const validRedirects = Array.isArray(redirects)
-    ? redirects.filter(
-        (redirect) =>
-          redirect.source && redirect.destination && typeof redirect.permanent === 'boolean',
-      )
-    : []
+  // Filter out invalid redirects (those missing 'source: string', 'destination:string', or 'statusCode: number')
+  const validRedirects = redirects.filter(
+    (redirect) =>
+      redirect.source &&
+      typeof redirect.source === 'string' &&
+      redirect.destination &&
+      typeof redirect.destination === 'string' &&
+      redirect.statusCode &&
+      typeof redirect.statusCode === 'number',
+  )
 
   // Return valid redirects if any, otherwise return an empty array
   if (validRedirects.length > 0) {
+    console.log(`Generated ${validRedirects.length} redirects`)
     return validRedirects
   }
 
+  console.log('Generated 0 redirects')
   return []
 }
