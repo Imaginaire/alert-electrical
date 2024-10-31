bd129ae9a89bb3b3adbc5becca81125db5519d47
Jessy Yeh <jessy@imaginaire.co.uk>
2024-10-31 08:39:18 +0000

    chore: remove breadcrumb provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,7 +5,6 @@ import {Cormorant_Infant} from 'next/font/google'
 import {Manrope} from 'next/font/google'
 import {CartProvider} from '@/contexts/CartContext'
 import PreviewProvider from '@/components/preview/PreviewProvider'
-import {BreadcrumbProvider} from '@/contexts/BreadcrumbContext'
 
 const cormorantInfant = Cormorant_Infant({
   subsets: ['latin'],
@@ -34,16 +33,14 @@ export default function App({Component, pageProps}: AppProps) {
         </PreviewProvider>
       ) : (
         <CartProvider>
-          <BreadcrumbProvider>
-            <main
-              className={`${cormorantInfant.className} ${cormorantInfant.variable} ${manropes.variable}`}
-            >
-              <Component {...pageProps} />
-              {pageProps?.settings?.googleTagManager && (
-                <GoogleTagManager gtmId={pageProps.settings.googleTagManager} />
-              )}
-            </main>
-          </BreadcrumbProvider>
+          <main
+            className={`${cormorantInfant.className} ${cormorantInfant.variable} ${manropes.variable}`}
+          >
+            <Component {...pageProps} />
+            {pageProps?.settings?.googleTagManager && (
+              <GoogleTagManager gtmId={pageProps.settings.googleTagManager} />
+            )}
+          </main>
         </CartProvider>
       )}
     </>
