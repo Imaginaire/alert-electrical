f985d72874f14710e60966cbe2d645b95be6b412
Jessy Yeh <jessy@imaginaire.co.uk>
2024-11-13 09:08:07 +0000

    fix: newletter container width

--- frontend/components/global/Footer/Newsletter.tsx
+++ frontend/components/global/Footer/Newsletter.tsx
@@ -3,13 +3,13 @@ import {Newsletter as NewsletterType} from '@/types'
 export default function Newsletter({newsletter}: {newsletter: NewsletterType}) {
   return (
     <div className="bg-secondary-grey px-7 py-12 ">
-      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center ">
-        <div className=" flex flex-col gap-1 text-center lg:text-left flex-1">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row md:gap-5 items-center justify-center ">
+        <div className=" flex flex-col gap-1 text-center md:text-left flex-1">
           <h2 className=" text-secondary-grey-text text-2xl font-bold">{newsletter.heading}</h2>
           <p>{newsletter.subHeading}</p>
         </div>
 
-        <div className="flex min-w-[500px] max-w-4xl lg:w-full flex-1 mt-7 lg:mt-0">
+        <div className="flex w-full max-w-4xl lg:w-full flex-1 mt-7 md:mt-0">
           <div className="relative flex-grow ">
             <input
               type="email"
